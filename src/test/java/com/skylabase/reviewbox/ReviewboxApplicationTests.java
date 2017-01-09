0e9aa096cd5f54a53b8176f31d0853a7529270c0
Yannick Awasum <yannickawasum@gmail.com>
2017-01-09 16:02:39 +0100

    init spring boot default (#1)

---
+++ src/test/java/com/skylabase/reviewbox/ReviewboxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.skylabase.reviewbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
